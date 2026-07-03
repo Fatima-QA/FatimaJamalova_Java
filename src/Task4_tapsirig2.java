81cf2f914d18d9b84757a5690f8ec6c76aeff95c
Fatima Jamalova <camalovafatime2003@gmail.com>
2026-07-03 08:16:24 +0400

    Kredit Hesabı əlavə edildi

--- src/Task4_tapsirig2.java
+++ src/Task4_tapsirig2.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Task4_tapsirig2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.print("Kredit məbləği: ");
+        double mebleg = sc.nextDouble();
+
+        System.out.print("İllik faiz (%): ");
+        double faiz = sc.nextDouble();
+
+        System.out.print("Müddət (ay): ");
+        int muddet = sc.nextInt();
+
+        // hesablamalar
+        double umumiFaiz = mebleg * (faiz / 100);
+        double umumiOdenis = mebleg + umumiFaiz;
+        double ayliqOdenis = umumiOdenis / muddet;
+
+        // nəticə
+        System.out.println("\n--- Kredit Hesabatı ---");
+        System.out.println("Kredit məbləği: " + mebleg);
+        System.out.println("İllik faiz: " + faiz + "%");
+        System.out.println("Müddət: " + muddet + " ay");
+
+        System.out.println("Ümumi faiz: " + umumiFaiz);
+        System.out.println("Ümumi ödəniş: " + umumiOdenis);
+        System.out.println("Aylıq ödəniş: " + ayliqOdenis);
+    }
 }
